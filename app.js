16e9d262a05fdccb9e23e03d1f603993d13e0664
BuildTools <unconfigured@null.spigotmc.org>
2020-03-03 20:45:47 -0300

    Consertando grupos VRP

--- app.js
+++ app.js
@@ -107,6 +107,9 @@ async function addGroupVRP(id, group) {
   const res = await sql("SELECT dvalue FROM vrp_user_data WHERE user_id='"+id+"' AND dkey='vRP:datatable'");
   if (res.length > 0) {
     const data = JSON.parse(res[0].dvalue);
+    if (data.groups.isArray()) {
+      data.groups = {};
+    }
     data.groups[group] = true;
     sql("UPDATE vrp_user_data SET dvalue=? WHERE user_id=?", [JSON.stringify(data), id]);
     return true;
@@ -122,10 +125,10 @@ async function removerGrupoVRP(id, group) {
 
 async function removeGroupVRP(id, group) {
   if (isOnline(id)) return false;
-  const res = await sql("SELECT dvalue FROM vrp_user_data WHERE user_id='"+id+"' AND dkey='vRP:datatable'");
+  const res = await sql("SELECT dvalue FROM vrp_user_data WHERE user_id=? AND dkey='vRP:datatable'", [id]);
   if (res.length > 0) {
     const data = JSON.parse(results[0]);
-    delete data.groups[group];
+    if (!data.groups.isArray()) delete data.groups[group];
     sql("UPDATE vrp_user_data SET dvalue=? WHERE user_id=?", [JSON.stringify(data), id]);
     return true;
   } else {
