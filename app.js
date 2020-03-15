a02f174b0b274899e07ea37945a9eec26b5c563f
jesteriruka <jesteriruka@gmail.com>
2020-03-14 22:35:39 -0300

    Fixing module reserved word

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const config = require('./src/config');
 let scheduled = JSON.parse(fs.readFileSync('./scheduled.json'));
 let playerList = [];
 
-let module = { isOnline, sql, config, DEBUG };
+let mod = { isOnline, sql, config, DEBUG };
 
-const vrp = require('./src/vrp')(module);
-const esx = require('./src/esx')(module);
+const vrp = require('./src/vrp')(mod);
+const esx = require('./src/esx')(mod);
 
 let DEBUG = false;
 
