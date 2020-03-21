95370a7c8c034ba7bd4326e0ed4d654184046ab6
jesteriruka <jesteriruka@gmail.com>
2020-03-21 15:23:22 -0300

    AutoUpdater

--- update.js
+++ update.js
@@ -1,4 +1,16 @@
-const fs = require('fs');
-const unzip = require('unzip');
+const fs = require('graceful-fs');
+const unzipper = require('unzipper');
+const nodefetch = require('node-fetch');
 
-fs.createReadStream('https://github.com/JesterIruka/fivem-delivery/archive/master.zip').pipe(unzip.Extract({ path: '.' }));
+nodefetch('https://github.com/JesterIruka/fivem-delivery/archive/master.zip').then(res => {
+    res.body.pipe(unzipper.Parse()).on('entry', function (entry) {
+        const fileName = entry.path.replace('fivem-delivery-master', '.');
+        const type = entry.type; // 'Directory' or 'File'
+        const size = entry.vars.uncompressedSize; // There is also compressedSize;
+        if (type == 'File') {
+          entry.pipe(fs.createWriteStream(fileName));
+        } else {
+          entry.autodrain();
+        }
+      });;
+});
