45561350a1fffa0b165876e64de07ec8a2253542
JesterIruka <jesteriruka@gmail.com>
2020-05-17 17:34:05 -0300

    Refactoring

--- src/database.js
+++ src/database.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 const config = require('./config');
 const webhook = require('./webhook');
 
-const link = mysql.createConnection({...config, debug: false});
+const link = mysql.createConnection({ ...config, debug: false });
 
-async function sql(sql, values=[], ignoreError=false, debug=true) {
-  return await new Promise((resolve,reject) => {
+async function sql(sql, values = [], ignoreError = false, debug = true) {
+  return await new Promise((resolve, reject) => {
     if (debug) webhook.debug(`Executando SQL\n${sql}\n[${values.join(",")}]`)
-    link.query(sql, values, (err,results) => {
+    link.query(sql, values, (err, results) => {
       if (err) {
-        webhook.debug('Erro em '+sql+(ignoreError?' (Ignorado)':'')+'\n'+err, true);
+        webhook.debug('Erro em ' + sql + (ignoreError ? ' (Ignorado)' : '') + '\n' + err, true);
         if (ignoreError) resolve([]);
         else reject(err);
       } else resolve(results);
@@ -26,4 +26,4 @@ async function queryTables() {
 
 const getTables = () => tables;
 
-module.exports = {sql, link, queryTables, getTables};
+module.exports = { sql, link, queryTables, getTables };
--- src/webhook.js
+++ src/webhook.js
@@ -5,18 +5,18 @@ let client;
 
 if (config.extras && config.extras.webhook) {
   if (url = config.extras.webhook.url) {
-    const [id,token] = url.replace('https://discordapp.com/api/webhooks/', '').split('/');
+    const [id, token] = url.replace('https://discordapp.com/api/webhooks/', '').split('/');
     client = new Discord.WebhookClient(id, token);
   } else {
-    const {id,token} = config.extras.webhook;
+    const { id, token } = config.extras.webhook;
     client = new Discord.WebhookClient(id, token);
   }
 } else if (config.discord_webhook && config.discord_webhook.includes('discordapp.com')) {
-  const [id,token] = config.discord_webhook.replace('https://discordapp.com/api/webhooks/', '').split('/');
+  const [id, token] = config.discord_webhook.replace('https://discordapp.com/api/webhooks/', '').split('/');
   client = new Discord.WebhookClient(id, token);
 }
 
-function broadcast(text, error=false) {
+function broadcast(text, error = false) {
   let embed = new Discord.MessageEmbed();
   embed.setDescription(text);
   if (error) embed.setColor('DARK_RED')
@@ -25,26 +25,26 @@ function broadcast(text, error=false) {
   client.send(embed);
 }
 
-function debug(message, error=false, ignoreable=false) {
+function debug(message, error = false, ignoreable = false) {
   if (client && !ignoreable) broadcast(message, error);
   else if (error) console.error(message);
   else if (config.debug) console.log(message);
 }
 
-function dataAtualFormatada(){
+function dataAtualFormatada() {
   var data = new Date(),
-      dia  = data.getDate().toString(),
-      diaF = (dia.length == 1) ? '0'+dia : dia,
-      mes  = (data.getMonth()+1).toString(),
-      mesF = (mes.length == 1) ? '0'+mes : mes,
-      anoF = data.getFullYear(),
-      hora = data.getHours(),
-      horaF = (hora < 10) ? '0'+hora: hora,
-      min = data.getMinutes(),
-      minF = (min < 10) ? '0'+min: min,
-      seg = data.getSeconds(),
-      segF = (seg < 10) ? '0'+seg: seg;
-  return diaF+"/"+mesF+"/"+anoF+" "+horaF+":"+minF+":"+segF;
+    dia = data.getDate().toString(),
+    diaF = (dia.length == 1) ? '0' + dia : dia,
+    mes = (data.getMonth() + 1).toString(),
+    mesF = (mes.length == 1) ? '0' + mes : mes,
+    anoF = data.getFullYear(),
+    hora = data.getHours(),
+    horaF = (hora < 10) ? '0' + hora : hora,
+    min = data.getMinutes(),
+    minF = (min < 10) ? '0' + min : min,
+    seg = data.getSeconds(),
+    segF = (seg < 10) ? '0' + seg : seg;
+  return diaF + "/" + mesF + "/" + anoF + " " + horaF + ":" + minF + ":" + segF;
 }
 
 module.exports = { debug };
