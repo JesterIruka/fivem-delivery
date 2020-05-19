96c8db71c1f5cfd76ed11f82e82a9ec8cc092ef0
JesterIruka <jesteriruka@gmail.com>
2020-05-19 05:07:43 -0300

    new methods

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,35 @@
 const config = require("./config");
 
-module.exports.americandreamDate = (date) => {
-  const d = date.getDate().toString().padStart(2, '0');
-  const M = (date.getMonth()+1).toString().padStart(2, '0');
-  const y = date.getFullYear()-2000;
+function twoDigits(n) {
+  return n.toString().padStart(2, '0');
+}
 
-  const h = date.getHours().toString().padStart(2, '0');
-  const m = date.getMinutes().toString().padStart(2, '0');
+module.exports.currentDateFormatted = (date = null) => {
+  if (typeof date === 'number') date = new Date(date);
+  if (!date) date = new Date();
+
+  const day = twoDigits(date.getDate());
+  const month = twoDigits(date.getMonth() + 1);
+  const year = date.getFullYear();
+
+  const hour = twoDigits(date.getHours());
+  const minutes = twoDigits(date.getMinutes());
+  const seconds = twoDigits(date.getSeconds());
+
+  return `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`;
+}
+
+module.exports.americandreamDate = (date) => {
+  const d = twoDigits(date.getDate());
+  const M = twoDigits(date.getMonth() + 1);
+  const y = date.getFullYear() - 2000;
+
+  const h = twoDigits(date.getHours());
+  const m = twoDigits(date.getMinutes());
 
   return str = `${d}/${M}/${y} ${h}:${m}`;
 }
+
 module.exports.hasPlugin = (name) => {
   return config.extras && config.extras.plugins && config.extras.plugins.includes(name);
 }
