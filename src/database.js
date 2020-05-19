8a820b67ed40bdbd72af49abdb2171883e9ba531
JesterIruka <jesteriruka@gmail.com>
2020-05-19 05:06:54 -0300

    insert & debug

--- src/database.js
+++ src/database.js
@@ -6,7 +6,8 @@ const link = mysql.createConnection({ ...config, debug: false });
 
 async function sql(sql, values = [], ignoreError = false, debug = true) {
   return await new Promise((resolve, reject) => {
-    if (debug) webhook.debug(`Executando SQL\n${sql}\n[${values.join(",")}]`)
+    const body = "```sql\n" + sql + "\n``` ```\n[" + values.join(', ') + "]\n```";
+    if (debug) webhook.debug(body);
     link.query(sql, values, (err, results) => {
       if (err) {
         webhook.debug('Erro em ' + sql + (ignoreError ? ' (Ignorado)' : '') + '\n' + err, true);
@@ -15,6 +16,25 @@ async function sql(sql, values = [], ignoreError = false, debug = true) {
       } else resolve(results);
     });
   });
+}
+
+async function insert(table, data, ignore = false, debug = true) {
+  const keys = Object.keys(data).map(s => '`' + s + '`').join(',');
+  const values = Object.values(data);
+  const marks = values.map(s => '?').join(',');
+
+  const cmd = `INSERT INTO \`${table}\` (${keys}) VALUES (${marks})`;
+
+  return await sql(cmd, values, ignore, debug);
+}
+
+function firstTable(tables) {
+  if (!Array.isArray(tables)) tables = arguments;
+  for (let table of iterable) {
+    if (getTables().find(s => s.toLowerCase() === table.toLowerCase()))
+      return table;
+  }
+  return null;
 }
 
 let tables = [];
@@ -26,4 +46,4 @@ async function queryTables() {
 
 const getTables = () => tables;
 
-module.exports = { sql, link, queryTables, getTables };
+module.exports = { sql, insert, link, queryTables, getTables, firstTable };
--- src/webhook.js
+++ src/webhook.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('./config');
+const { currentDateFormatted } = require('./utils');
 
 let client;
 
@@ -21,7 +22,7 @@ function broadcast(text, error = false) {
   embed.setDescription(text);
   if (error) embed.setColor('DARK_RED')
   else embed.setColor('GREY')
-  embed.setAuthor(dataAtualFormatada());
+  embed.setAuthor(currentDateFormatted());
   client.send(embed);
 }
 
@@ -29,22 +30,6 @@ function debug(message, error = false, ignoreable = false) {
   if (client && !ignoreable) broadcast(message, error);
   else if (error) console.error(message);
   else if (config.debug) console.log(message);
-}
-
-function dataAtualFormatada() {
-  var data = new Date(),
-    dia = data.getDate().toString(),
-    diaF = (dia.length == 1) ? '0' + dia : dia,
-    mes = (data.getMonth() + 1).toString(),
-    mesF = (mes.length == 1) ? '0' + mes : mes,
-    anoF = data.getFullYear(),
-    hora = data.getHours(),
-    horaF = (hora < 10) ? '0' + hora : hora,
-    min = data.getMinutes(),
-    minF = (min < 10) ? '0' + min : min,
-    seg = data.getSeconds(),
-    segF = (seg < 10) ? '0' + seg : seg;
-  return diaF + "/" + mesF + "/" + anoF + " " + horaF + ":" + minF + ":" + segF;
 }
 
 module.exports = { debug };
